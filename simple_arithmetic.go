f5394b6ef1a285aafe22b070902438da31ee7058
Tyler Coil <kamori@tylers-mbp.attlocal.net>
2018-09-13 00:33:10 -0500

    fixing linting alerts

--- simple_arithmetic.go
+++ simple_arithmetic.go
@@ -1,17 +1,25 @@
 package main
 
+// Add - Exported, a+b returns int
 func Add(a, b int) int {
+	// TODO: test with floats
 	return a + b
 }
 
+// Subtract - Exported, a-b return int
 func Subtract(a, b int) int {
+	// TODO: test with floats
 	return a - b
 }
 
+// Multiply - Exported, take a * b and return as int
 func Multiply(a, b int) int {
+	// TODO What about floats?
 	return a * b
 }
 
+// Divide - Exported, take a / b and returns as int
 func Divide(a, b int) int {
+	// TODO: Turn this into a float and see how it affects tests
 	return a / b
 }
